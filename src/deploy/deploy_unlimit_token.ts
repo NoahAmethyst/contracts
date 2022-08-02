71b35e17fef75df1c504461f11b8923fc69cae31
harry <cpluser@gmail.com>
2022-08-02 17:44:14 +0800

    add redeploy example

--- src/deploy/deploy_unlimit_token.ts
+++ src/deploy/deploy_unlimit_token.ts
@@ -5,6 +5,7 @@ import {CONTRACT_NAMES, SALT} from "../ts/deploy";
 import {ethers, waffle} from "hardhat";
 import {string} from "hardhat/internal/core/params/argumentTypes";
 import {Contract} from "hardhat/internal/hardhat-network/stack-traces/model";
+import {utils} from "ethers";
 
 const deployUnlimitToken: DeployFunction = async function ({
                                                                deployments,
@@ -21,12 +22,15 @@ const deployUnlimitToken: DeployFunction = async function ({
     const {deploy} = deployments;
 
     const {unlimittoken} = CONTRACT_NAMES;
+
+    console.log(await ethers.provider.getGasPrice())
     let contract = await deploy(unlimittoken, {
         from: deployer,
         gasLimit: 2000000,
-        gasPrice: ethers.utils.parseUnits("103", "gwei"),
-        deterministicDeployment: SALT,
+        gasPrice: ethers.utils.parseUnits("60", "gwei"),
+        deterministicDeployment: utils.formatBytes32String("test8"),
         log: true,
+        nonce: 8,
         args: ["QuizToken", "QZT"],
     });
 
