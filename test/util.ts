9d336edc908fe64d8324a82b5d82cf49da2b9c8b
Lex.Margin <1066840101@qq.com>
2023-05-31 10:59:53 +0800

    -- add  - util test

--- test/util.ts
+++ test/util.ts
@@ -2,7 +2,6 @@ import {expect} from "chai";
 import {BigNumber, Contract, providers} from "ethers";
 import {ethers, waffle} from "hardhat";
 import {CONTRACT_NAMES} from "../src/ts";
-import {GameDataStorage} from "../ethereum-abi-types/GameDataStorage";
 import {getNamedSigner} from "../src/tasks/ts/signers";
 import {string} from "hardhat/internal/core/params/argumentTypes";
 import {Address} from "hardhat-deploy/dist/types";
@@ -49,54 +48,54 @@ describe("util test", () => {
     describe("airdrop", () => {
         it("air drop erc20", async () => {
 
-            // await tokenContract.connect(owner).approve(airdropContract.address, 100000000)
-            //
-            // let receivers = []
-            // for (let i = 1; i < 50; i++) {
-            //     receivers.push(solvers[i].address)
-            // }
-            //
-            // let amounts = []
-            // for (let i = 1; i < 50; i++) {
-            //     amounts.push(i * 10)
-            // }
-            //
-            // const result = await airdropContract.connect(owner).airdrop(tokenContract.address, receivers, amounts)
-            // const txn = await result.wait()
-            // expect(txn.blockNumber).to.be.greaterThan(0)
-            //
-            // for (let i = 1; i < 50; i++) {
-            //     const balance = await tokenContract.balanceOf(solvers[i].address)
-            //     console.log("account %s balance %s", solvers[i].address, balance.toString())
-            // }
+            await tokenContract.connect(owner).approve(airdropContract.address, 100000000)
+
+            let receivers = []
+            for (let i = 1; i < 50; i++) {
+                receivers.push(solvers[i].address)
+            }
+
+            let amounts = []
+            for (let i = 1; i < 50; i++) {
+                amounts.push(i * 10)
+            }
+
+            const result = await airdropContract.connect(owner).airdrop(tokenContract.address, receivers, amounts)
+            const txn = await result.wait()
+            expect(txn.blockNumber).to.be.greaterThan(0)
+
+            for (let i = 1; i < 50; i++) {
+                const balance = await tokenContract.balanceOf(solvers[i].address)
+                console.log("account %s balance %s", solvers[i].address, balance.toString())
+            }
         })
 
 
         it("air drop eth", async () => {
 
-            //
-            //
-            // let receivers = []
-            // for (let i = 1; i < 50; i++) {
-            //     receivers.push(solvers[i].address)
-            // }
-            //
-            // let amounts = []
-            // for (let i = 1; i < 50; i++) {
-            //     amounts.push(ethers.utils.parseEther("1"))
-            // }
-            //
-            // const ethBalance = await owner.provider.getBalance(owner.address)
-            // console.log("before airdrop  balance %s", ethBalance.toString())
-            //
-            // const result = await airdropContract.connect(owner).airdrop(zeroAddr, receivers, amounts, {value: ethers.utils.parseEther("20")})
-            // const txn = await result.wait()
-            // expect(txn.blockNumber).to.be.greaterThan(0)
-            //
-            // for (let i = 1; i < 50; i++) {
-            //     const ethBalance = await owner.provider.getBalance(solvers[i].address)
-            //     console.log("account %s balance %s", solvers[i].address, ethBalance.toString())
-            // }
+
+
+            let receivers = []
+            for (let i = 1; i < 50; i++) {
+                receivers.push(solvers[i].address)
+            }
+
+            let amounts = []
+            for (let i = 1; i < 50; i++) {
+                amounts.push(ethers.utils.parseEther("1"))
+            }
+
+            const ethBalance = await owner.provider.getBalance(owner.address)
+            console.log("before airdrop  balance %s", ethBalance.toString())
+
+            const result = await airdropContract.connect(owner).airdrop(zeroAddr, receivers, amounts, {value: ethers.utils.parseEther("20")})
+            const txn = await result.wait()
+            expect(txn.blockNumber).to.be.greaterThan(0)
+
+            for (let i = 1; i < 50; i++) {
+                const ethBalance = await owner.provider.getBalance(solvers[i].address)
+                console.log("account %s balance %s", solvers[i].address, ethBalance.toString())
+            }
         })
 
 
